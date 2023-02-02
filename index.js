5c6386f3788f2c6d7728996f3b022a103f66e7cf
unknown <acbarbosa.neto@gmail.com>
2023-02-02 13:50:37 -0300

    fixed express static

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
 
 
 app.get("/", function(req,res){
